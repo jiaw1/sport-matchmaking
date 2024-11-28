6f378081c4c5b42369973457a0ed3ee9bf82c0f7
sneaktricks <168283927+sneaktricks@users.noreply.github.com>
2024-11-28 22:11:23 +0200

    Add test sign in button

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
+"use client";
+
 // import * as mockData from "@/app/lib/mockData";
 import AppHeader from "./components/typography/AppHeader";
 import SearchBar from "./components/SearchBar";
 import SectionHeader from "./components/typography/SectionHeader";
 import EventCardsList from "./components/EventCardsList";
 import { Box } from "@mui/material";
+import { signIn, useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
   // const upcomingEvent = mockData.activityEvents.find(event => event.sport === "Tennis");
   // const recommendedEvent = mockData.activityEvents.find(event => event.sport === "Badminton");
 
   return (
     <div>
-      <Box sx={{mb:3}}>
+      <Box sx={{ mb: 3 }}>
         <AppHeader>Hello John</AppHeader>
       </Box>
+      {/* TODO: REMOVE THIS TEST SIGN IN BUTTON */}
+      <button onClick={() => signIn()}>Sign in {!!session}</button>
       <SearchBar></SearchBar>
       <SectionHeader>Upcoming matches</SectionHeader>
       <EventCardsList></EventCardsList>
